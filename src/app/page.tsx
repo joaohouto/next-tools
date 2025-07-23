21950deefd67dd0bfc976f3cf7ead659c855d358
João <joaocouto.jar@gmail.com>
2025-07-23 12:49:56 -0400

    idk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import Image from "next/image";
-import useRouter from "next/router";
-import { motion } from "framer-motion";
+import { useRouter } from "next/router";
+import { motion } from "motion/react";
 
 import { useIsMobile } from "@/hooks/use-mobile";
 import { PAGE_LIST } from "@/config/page-list";
--- src/components/command-menu.tsx
+++ src/components/command-menu.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as React from "react";
-import useRouter from "next/router";
+import { useRouter } from "next/router";
 
 import { useHotkeys } from "react-hotkeys-hook";
 
