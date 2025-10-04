4ba055903663908aaef75d89f5370866608ed085
João <joaocouto.jar@gmail.com>
2025-10-04 17:57:35 -0400

    idk

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  serverExternalPackages: ["tesseract.js"],
-  outputFileTracingIncludes: {
-    "/api/**/*": ["./node_modules/**/*.wasm", "./node_modules/**/*.proto"],
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
